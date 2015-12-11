a31f4665f9244cd062ab2b0c0546c59a31fa2c37
Fedor Baart <fedor.baart@deltares.nl>
2015-12-10 22:33:47 -0800

    base on gulp-webapp

---
+++ generators/app/templates/main.js
@@ -0,0 +1,25 @@
+(function(){
+  'use strict';
+  var onPointCloudLoaded = function(event){
+    // do stuff here that should be executed whenever a point cloud has been loaded.
+    // event.pointcloud returns the point cloud object
+    console.log("a point cloud has been loaded", event);
+  };
+
+  var viewer = new Potree.Viewer(document.getElementById('potree_render_area'), {
+    onPointCloudLoaded: onPointCloudLoaded
+  });
+
+  viewer.setEDLEnabled(false);
+  viewer.setPointSize(3);
+  viewer.setMaterial('RGB');
+  viewer.setFOV(60);
+  viewer.setPointSizing('Fixed');
+  viewer.setQuality('Squares');
+  viewer.setPointBudget(1 * 1000 * 1000);
+  viewer.setDescription('');
+  viewer.addPointCloud('http://5.9.65.151/mschuetz/potree/resources/pointclouds/opentopography/CA13_1.4/cloud.js');
+  viewer.loadGUI();
+
+
+})();
--- test/app.js
+++ test/app.js
@@ -6,17 +6,44 @@ var helpers = require('yeoman-generator').test;
 describe('generator-potree:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
+      .withOptions({
+        'test-framework': 'mocha'
+      })
+      .withPrompts({
+        features: ['includeSass', 'includeBootstrap', 'includeModernizr'],
+        includeJQuery: true
+      })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt',
       'bower.json',
       'gulpfile.babel.js',
       'app/index.html'
+    ]);
+  });
+});
+
+describe('generator-potree:app no features', function () {
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withOptions({
+        'test-framework': 'jasmine'
+      })
+      .withPrompts({
+        features: [],
+        includeJQuery: false
+      })
+      .on('end', done);
+  });
+
+  it('creates files', function () {
+    assert.file([
+      'bower.json',
+      'gulpfile.babel.js',
+      'app/index.html'
+
     ]);
   });
 });
